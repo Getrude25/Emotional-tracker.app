b682c6a2abf7ebb5cd26bbc6bf78a169056179cb
Getrude25 <getrudavitus2000@gmail.com>
2025-09-02 19:37:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SK8UytTLcbyHwNXU4CGBzAyK9kPg1PfcRnpc41glXK'
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key-here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     
     # Database configuration
@@ -19,4 +19,5 @@ class Config:
     CLICKPESA_BASE_URL = os.environ.get('CLICKPESA_BASE_URL') or 'https://api.clickpesa.com'
     
     # Hugging Face configuration
+
     HUGGINGFACE_API_KEY = os.environ.get('HUGGINGFACE_API_KEY')
